ca55a7bb662a1d1a017e460d74cc4707c1886406
achrefbs <siachrefboularess@gmail.com>
2020-02-05 13:42:54 +0000

    ex9 update3

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  *main - prints exactly a text and return 1
- *Return: 0
+ *Return: 1
  */
 int main(void)
 {
-write("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
 return (1);
 }
