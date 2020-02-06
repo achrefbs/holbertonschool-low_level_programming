44e989851d3875ee3c38b06513539afc31ddec74
achrefbs <siachrefboularess@gmail.com>
2020-02-06 14:18:07 +0000

    ex6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - print the numbers of base 10
+ *Return: 0
+ */
+int main(void)
+{
+int b;
+for (b = 48; b <= 57; b++)
+{
+putchar(b);
+}
+putchar('\n');
+return (0);
+}
