21032eb01276a31ade68aa156127e166471ea03b
achrefbs <siachrefboularess@gmail.com>
2020-02-06 14:02:27 +0000

    ex5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - prints all numbers of base 0
+ *Return: 0
+ */
+int main(void)
+{
+int b;
+for (b = 0; b <= 9; b++)
+{
+printf('%d', b);
+}
+printf('\n');
+return (0);
+}
