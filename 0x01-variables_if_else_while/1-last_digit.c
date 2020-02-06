c9048384304159acbcebe45e89145bc118216c5a
achrefbs <siachrefboularess@gmail.com>
2020-02-06 12:44:50 +0000

    ex1 update

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,15 +14,15 @@ n = rand() - RAND_MAX / 2;
 lastdig = n % 10;
 if (lastdig > 5)
 {
-printf("last digit of %d is %d and is greater than 5\n", n, lastdig);
+printf("Last digit of %d is %d and is greater than 5\n", n, lastdig);
 }
 else if (lastdig == 0)
 {
-printf("last digit of %d is %d and is 0\n", n, lastdig);
+printf("Last digit of %d is %d and is 0\n", n, lastdig);
 }
 else
 {
-printf("last digit of %d is %d and is less than 6 and not 0\n", n, lastdig);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdig);
 }
 return (0);
 }
