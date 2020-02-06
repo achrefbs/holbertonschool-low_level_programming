9dc6575b1d8a069ee35c19b7014b575e178e5ce0
achrefbs <siachrefboularess@gmail.com>
2020-02-06 17:19:28 +0000

    ex9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ *main - print all combinations of single-digit
+ *Return: 0
+ */
+int main(void)
+{
+int n;
+for (n = 48; n <= 57; n++)
+{
+putchar(n);
+if (n == 57)
+{
+putchar('\n');
+}
+else
+{
+putchar(',');
+putchar(' ');
+}
+}
+return (0);
+}
