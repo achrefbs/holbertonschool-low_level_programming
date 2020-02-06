145095c98716572e4c37c38374d9e18059753171
achrefbs <siachrefboularess@gmail.com>
2020-02-06 13:16:50 +0000

    ex4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - prints alphabet exept q qnd e
+ *Return: 0;
+ */
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+if (c != 'q' && c != 'e')
+{
+putchar(c);
+}
+}
+return (0);
+}
