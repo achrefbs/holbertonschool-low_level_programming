fb97a84002dde0b014ffa759d248fdb1f1aa24e2
achrefbs <siachrefboularess@gmail.com>
2020-02-11 08:16:11 +0000

    ex0 update

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,20 +1,20 @@
 #include "holberton.h"
 #include <string.h>
 /**
- *
- *
+ *main - pritns Holberton
+ *Return: 0
  */
 int main(void)
 {
-  _putchar('H');
-  _putchar('o');
-  _putchar('l');
-  _putchar('b');
-  _putchar('e');
-  _putchar('r');
-  _putchar('t');
-  _putchar('o');
-  _putchar('n');
-  _putchar('\n');
+_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('n');
+_putchar('\n');
 return (0);
 }
