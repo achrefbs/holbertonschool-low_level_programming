2d5912119e8af712c8fe5a3980fd252f9cad3e62
achrefbs <siachrefboularess@gmail.com>
2020-02-11 14:01:49 +0000

    ex7 update5

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,12 +11,12 @@ if (x < 0)
 x *= -1;
 x = x % 10;
 _putchar(x + '0');
-return ((x % 10) + '0');
+return (x);
 }
 else
 {
 x = x % 10;
 _putchar(x + '0');
-return ((x % 10) * -1);
+return (x);
 }
 }
