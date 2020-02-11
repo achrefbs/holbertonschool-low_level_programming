a9c0315c56e6ab3d575d19a79e47791a104dc435
achrefbs <siachrefboularess@gmail.com>
2020-02-11 18:00:19 +0000

    ex11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ *
+ *
+ *
+ */
+void print_to_98(int n)
+{
+int i;
+if (n == 98)
+{
+printf("%d\n", n);
+}
+else if (n < 98)
+{
+for (i = n; i < 98; i++)
+{
+printf("%d, ",i);
+}
+printf("98\n");
+}
+else 
+{
+for(i = n; i > 98; i--)
+{
+printf("%d, ",i);
+}
+printf("98\n");
+}
+}
