19fc1106adcb8a7e7eb8867d8c4bc565543e0961
achrefbs <siachrefboularess@gmail.com>
2020-02-11 09:22:37 +0000

    ex3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ *_islower - checks for lowercase character
+ *Return: 0
+ *@c: integer
+ */
+int _islower(int c)
+{
+if (c >= 97)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
