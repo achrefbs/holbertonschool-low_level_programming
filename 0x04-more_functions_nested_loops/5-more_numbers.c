f27353d1b3d4db89fc602647c030a7ae18fcad9d
achrefbs <siachrefboularess@gmail.com>
2020-02-13 12:27:21 +0000

    ex5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * more_numbers - prints from 1 to 14 (15 times)
+ *
+ * Return: Always 0.
+ */
+void more_numbers(void)
+{
+int i, x;
+for (i = 0; i <= 10; i++)
+{
+for (x = 0; x <= 14; x++)
+{
+if (x >= 10)
+{
+_putchar(x / 10 + '0');
+}
+_putchar(x % 10 + '0');
+}
+_putchar('\n');
+}
+}
