38fa594cab2170ef15eb5799dcfc33c2c60a2e1f
achrefbs <siachrefboularess@gmail.com>
2020-02-13 13:45:42 +0000

    ex8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * print_square - prints a square
+ *@size : integer
+ * Return: Always 0.
+ */
+void print_square(int size)
+{
+int i, j;
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for(i = 1; i <= size; i++)
+{
+for(j = 1; j <= size; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+}
