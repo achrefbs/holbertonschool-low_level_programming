5e4a429689943281486e1011d6b8b1f20ae00681
achrefbs <siachrefboularess@gmail.com>
2020-02-13 09:39:05 +0000

    ex0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *_isupper - checks for uppercase character
+ *@c : int
+ *Return: 0/1
+ */
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
