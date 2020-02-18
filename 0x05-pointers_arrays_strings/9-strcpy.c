e59c1ab3a36b104eb86154294835d1c501262f61
achrefbs <siachrefboularess@gmail.com>
2020-02-18 21:31:03 +0000

    ex9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+*_strcpy - copy a string
+*@dest: string
+*@src: string
+*Return: 0
+*/
+char *_strcpy(char *dest, char *src)
+{
+
+	if ((dest != '\0') && (src != '\0'))
+	{
+		while (*src != '\0')
+			*dest++ = *src++;
+		*dest = '\0';
+	}
+	return (dest);
+}
