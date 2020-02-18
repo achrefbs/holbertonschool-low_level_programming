86c3200892885425d662697f1129c4e680fe6a95
achrefbs <siachrefboularess@gmail.com>
2020-02-18 12:53:08 +0000

    ex8 update

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,11 +9,11 @@
 void print_array(int *a, int n)
 {
 int i;
-for (i = 0; i < n; i++)
+for (i = 0; i <= n; i++)
 {
-if (i != n - 1)
-printf("%d, ", a[i]);
-else
+if (i == n)
 printf("%d\n", a[i]);
+else
+printf("%d, ", a[i]);
 }
 }
