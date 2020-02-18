7283b640fc9314b65dbb138b5a8d5dbb38147333
achrefbs <siachrefboularess@gmail.com>
2020-02-18 12:49:27 +0000

    ex8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include"holberton.h"
+#include<stdio.h>
+/**
+ *print_array - prints array
+ *@a : array
+ *@n : integer
+ *Result: 0
+ */
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+if (i != n - 1)
+printf("%d, ", a[i]);
+else
+printf("%d\n", a[i]);
+}
+}
