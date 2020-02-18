49859745716fd4f3926bdd9dfa4009335d1be2fe
achrefbs <siachrefboularess@gmail.com>
2020-02-18 10:25:27 +0000

    ex5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include"holberton.h"
+/**
+ *rev_string - reverse string
+ *@s : string
+ *Return: 0
+ */
+void rev_string(char *s)
+{
+char h;
+int i, j;
+int c = 0;
+for (i = 0; s[i] != '\0'; i++)
+{
+c++;
+}
+for (j = 0 ; j <= c / 2 ; j++)
+{
+h = s[c - j - 1];
+s[c - j - 1] = s[j];
+s[j] = h;
+}
+}
