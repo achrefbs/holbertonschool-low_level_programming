e1927de4c25a75b40ab83e283726f5f8819556c4
achrefbs <siachrefboularess@gmail.com>
2020-02-19 11:31:50 +0000

    ex10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ *_atoi - convert into integer
+ *@s : string
+ *Return: 0
+ */
+int _atoi(char *s)
+{
+int i, n = 0, si = 1;
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] == '-')
+si = -1 * si;
+if (s[i] >= '0' && s[i] <= '9')
+{
+if ((s[i - 1] >= '0' && s[i - 1] <= '9') || n == 0)
+n = (n * 10) + s[i] - '0';
+else
+break;
+}
+}
+return (n *si);
+}
