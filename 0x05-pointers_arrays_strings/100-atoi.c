e9c383e33d07181cfbbf503ef2c038f4224fe192
achrefbs <siachrefboularess@gmail.com>
2020-02-19 11:40:34 +0000

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,18 +6,19 @@
  */
 int _atoi(char *s)
 {
-int i, n = 0, si = 1;
+int i, sign = 1;
+unsigned int n = 0;
 for (i = 0; s[i] != '\0'; i++)
 {
 if (s[i] == '-')
-si = -1 * si;
+sign *= -1;
 if (s[i] >= '0' && s[i] <= '9')
 {
 if ((s[i - 1] >= '0' && s[i - 1] <= '9') || n == 0)
-n = (n * 10) + s[i] - '0';
+n = (n * 10) + (s[i] - '0');
 else
 break;
 }
 }
-return (n *si);
+return (n *sign);
 }
