b013b8b221e009c7739d3ae434eb103b397a3538
achrefbs <siachrefboularess@gmail.com>
2020-02-19 09:09:48 +0000

    ex9 update

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,10 +7,11 @@
 */
 char *_strcpy(char *dest, char *src)
 {
-    int i;
+int i;
 for (i = 0; src[i] != '\0'; i++)
 {
-    dest[i] = src[i];
+dest[i] = src[i];
 }
+dest[i + 1] = '\0';
 return (dest);
 }
