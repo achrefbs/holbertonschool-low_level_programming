a906d0471077be2db69c601ea1be691d5dc4bf99
achrefbs <siachrefboularess@gmail.com>
2020-02-20 16:37:38 +0000

    ex0 update2

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,17 +1,19 @@
 #include "holberton.h"
 /**
-*
-*
-*
+**_strcat - concatenate 2 strings
+*@dest : string
+*@src : string
+*Return: 0
 */
 char *_strcat(char *dest, char *src)
 {
-int dl = 0;
-int i;
-for(i = 0; src[i] != '\0'; i++)
-dl++;
-for (i = 0 ;src[i] != '\0' ; i++)
-dest[dl + i] = src[i];
-dest[dl + i] = '\0';
+int i, j;
+for (i = 0; dest[i] != '\0'; i++)
+;
+for (j = 0; src[j] != '\0'; j++)
+{
+dest[i] = src[j];
+i++;
+}
 return (dest);
 }
