3bded439ef0210113cd8ba0eea3b88ffef6bae5c
achrefbs <siachrefboularess@gmail.com>
2020-02-20 17:08:16 +0000

    ex1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+**_strncat - concatenate 2 strings
+*@dest : string
+*@src : string
+*@n : integer
+*Return: 0
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+int i, j;
+for (i = 0; dest[i] != '\0'; i++)
+;
+for (j = 0; i < n && src[j] != '\0'; j++)
+{
+dest[i] = src[j];
+i++;
+}
+return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+char *_strncpy(char *dest, char *src, int n)
+{
+    char i;
+    for (i = 0; i = n; i++)
+    {
+        dest[i] = src[i] ;
+    }
+    
+}
