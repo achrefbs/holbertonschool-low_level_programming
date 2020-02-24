133e346c39097eb71a731a31cfb1ac8329aea90c
achrefbs <siachrefboularess@gmail.com>
2020-02-24 12:39:08 +0000

    ex1 update

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,5 +1,5 @@
 #include "holberton.h"
-/**
+/*
 **_memcpy - copies memory area.
 *@dest : pointer
 *@src : pointer
@@ -11,7 +11,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	unsigned int i;
 	for(i = 0; i < n; i++) 
 	{
-		*dest++ = *src++;
+		dest[i] = src[i];
 	}
 	return (dest);
 }
