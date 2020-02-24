1027220798bc82ca135db9c6b8284ee7e8778f65
achrefbs <siachrefboularess@gmail.com>
2020-02-24 12:14:30 +0000

    ex1 update

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,12 +8,10 @@
 */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	while (n > 0)
+	unsigned int i;
+	for(i = 0; i > n; i++) 
 	{
 		*dest = *src;
-		dest++;
-		src++;
-		n--;
 	}
 	return (dest);
 }
