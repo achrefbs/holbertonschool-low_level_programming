fb1172a858dcef272b6bf89b8d63039e1723aa56
achrefbs <siachrefboularess@gmail.com>
2020-03-01 17:13:03 +0000

    ex0 update

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include <stdio.h>
 /**
 *main -  a program that prints its name, followed by a new line.
 *@argc : int
@@ -7,8 +7,6 @@
 */
 int main(int argc, char *argv[])
 {
-while (*argv[0])
-_putchar(*argv[0]++);
-_putchar('\n');
+printf("%s\n", argv[0]);
 return (0);
 }
