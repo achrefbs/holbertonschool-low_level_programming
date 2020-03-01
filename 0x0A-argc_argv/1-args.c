fc2de096ae0d7a7c7b64dc885aa8d5c46f5fb8fd
achrefbs <siachrefboularess@gmail.com>
2020-03-01 17:22:46 +0000

    ex1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#define UNUSED(argv) (void)(argv)
 /**
 *main -  a program that prints the number of arguments passed into it.
 *@argc : int
@@ -7,6 +8,7 @@
 */
 int main(int argc, char *argv[])
 {	
+	UNUSED(argv);
 	printf("%d\n", argc - 1);
 	return (0);
 }
