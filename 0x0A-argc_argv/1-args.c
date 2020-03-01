6d5c61cb27533be05e9bf2cb05f9d0cab63de03c
achrefbs <siachrefboularess@gmail.com>
2020-03-01 17:17:54 +0000

    ex1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*main -  a program that prints the number of arguments passed into it.
+*@argc : int
+*@argv : pointer to a string
+*Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
