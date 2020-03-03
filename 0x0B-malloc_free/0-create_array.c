d7ac781718d316488714276c13c0508785543532
achrefbs <siachrefboularess@gmail.com>
2020-03-03 13:36:44 +0000

    ex0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+
+	if (size == 0)
+	return (NULL);
+	else
+	{
+		char *str;
+
+		str = malloc(sizeof(char) * size);
+		for (i = 0; i < size; i++)
+			{
+				str[i] = c;
+			}
+		return (str);
+		free(str);
+	}
+}
