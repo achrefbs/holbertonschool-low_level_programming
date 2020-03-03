ea0119b6a6bfb95131eabdf8d248587b9fb1d261
achrefbs <siachrefboularess@gmail.com>
2020-03-03 15:53:42 +0000

    ex2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,5 +8,24 @@
  */
 char *str_concat(char *s1, char *s2)
 {
+	int l1, i, j;
+	char *s;
+	s = malloc(sizeof(s1) + sizeof(s2));
+	if (s == NULL)
+	return (NULL);
+	for (l1 = 0; s1[l1] != '\0'; l1++)
+	;
+	for (i = 0; i <= l1; i++)
+	{
+		s[i] = s1[i];
+	}
+	for (j = l1; s2[j - l1] != '\0'; j++)
+	{
+		s[j] = s2[j - l1];
+	}
+	return(s);
+	
+	
+	
 	
 }
