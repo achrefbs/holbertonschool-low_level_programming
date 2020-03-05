e05fa1d5f2ab213e67dd2f781e32da2097705167
achrefbs <siachrefboularess@gmail.com>
2020-03-05 17:21:08 +0000

    update

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -8,7 +8,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	return (ptr);
 	if (new_size == 0 && ptr != NULL)
 	{
-		free(ptr);
 		return (NULL);
 	}
 	if (ptr == NULL)
@@ -16,10 +15,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		ptr = malloc(new_size);
 		return(ptr);
 	}
-	
-		tmp = malloc(new_size);
-		tmp = (char *)(ptr);
-	
-free (ptr);
+	tmp = malloc(new_size);
+	tmp = (char *)(ptr);
+free(ptr);
 return(tmp);
 }
