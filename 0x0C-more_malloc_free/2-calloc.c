37c99dbf3cb66fd3d67f3477b19bba838c78ebb7
achrefbs <siachrefboularess@gmail.com>
2020-03-05 14:49:08 +0000

    ex2 update

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,12 +9,17 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *a;
+	char *a;
+	unsigned int i;
 
 	if (size == 0 || nmemb == 0)
 	return (NULL);
-	a = malloc(nmemb * size);
+	a = malloc(size * nmemb);
 	if (a == NULL)
 	return (NULL);
+	for (i = 0; i < nmemb; i++)
+	{
+		a[i] = '\0';
+	}
 	return (a);
 }
