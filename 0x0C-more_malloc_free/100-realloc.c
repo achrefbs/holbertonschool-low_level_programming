501adc1da80eb7aa88bf17a24cdd8f552291852c
achrefbs <siachrefboularess@gmail.com>
2020-03-05 17:18:05 +0000

    ex4 update

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -4,7 +4,6 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *tmp;
-	unsigned int i;
 	if (new_size == old_size)
 	return (ptr);
 	if (new_size == 0 && ptr != NULL)
@@ -17,20 +16,9 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		ptr = malloc(new_size);
 		return(ptr);
 	}
-	if (old_size < new_size)
-	{
+	
 		tmp = malloc(new_size);
 		tmp = (char *)(ptr);
-	}
-	if (old_size > new_size)
-	{
-		tmp = malloc(new_size);
-		for (i = 0; i < new_size; i++)
-		{
-			tmp = (char *)(ptr);
-		}
-		
-	}
 	
 free (ptr);
 return(tmp);
