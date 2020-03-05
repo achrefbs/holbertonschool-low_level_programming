c76aa64b83125b7c0803472835ce22e1ece06d8e
achrefbs <siachrefboularess@gmail.com>
2020-03-05 11:49:55 +0000

    ex0 update

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,11 +1,17 @@
 #include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ **malloc_checked - a function that allocates memory using malloc.
+ *@b : int
+ *Return: pointer
+ */
 void *malloc_checked(unsigned int b)
 {
 	void *p;
+
 	p = malloc(b);
 	if (p == NULL)
 	exit(98);
-	return(p);
+	return (p);
 }
