33a122fee50e8b73803a7e1e5e30d27c9e6f04ba
achrefbs <achrefbs@github.com>
2020-03-14 18:23:54 +0000

    ex2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,5 +1,6 @@
 #include <stdarg.h>
 #include <stdio.h>
+#include "variadic_functions.h"
 /**
 *print_strings - a function that prints strings, followed by a new line.
 *@separator: pointer to a string
