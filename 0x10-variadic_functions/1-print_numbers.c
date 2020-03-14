58875942574784c9a5e14861e65dea24104714a0
achrefbs <achrefbs@github.com>
2020-03-14 12:47:12 +0000

    ex1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ *print_numbers - a function that prints numbers, followed by a new line.
+ *@separator : pointer to a string
+ *@n : unsigned int
+ *Return: NULL
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list list;
+
+	va_start(list, n);
+	if (separator != NULL)
+	for (i = 0; i < n; i++)
+	{
+		printf("%d%s", va_arg(list, int), separator);
+	}
+	else
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(list, int));
+	}
+	printf("\n");
+}
--- 0x10-variadic_functions/README.md
+++ 0x10-variadic_functions/README.md
@@ -1,1 +1,2 @@
 0 - a function that returns the sum of all its parameters.
+1 - a function that prints numbers, followed by a new line.
