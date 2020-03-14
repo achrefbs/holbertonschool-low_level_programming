f33f583ef6c12d6c98b38d7d1946765481021ca6
achrefbs <achrefbs@github.com>
2020-03-14 20:01:03 +0000

    ex4

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,13 +1,10 @@
 section .data
-
-    message db "Hello, Holberton", 0x0A
-
+	message db "Hello, Holberton", 0x0A
 section .text
-
-global _start
-_start:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, message
-    mov rdx, 17
-    syscall
+global main
+main:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, message
+	mov rdx, 17
+	syscall
