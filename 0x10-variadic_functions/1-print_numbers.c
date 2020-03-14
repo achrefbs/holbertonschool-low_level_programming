729e00ba782987b13d81fd6be9d4e013455ddc3d
achrefbs <achrefbs@github.com>
2020-03-14 14:07:48 +0000

    ex1 update

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,6 +11,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list list;
 
+	if (n == 0)
+	return (0);
 	va_start(list, n);
 	for (i = 0; i < n - 1 ; i++)
 	{
