bb55665097301b1ebb395b98b20f94aafd70fac9
achrefbs <siachrefboularess@gmail.com>
2020-03-22 11:27:33 +0000

    ex0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_list - a function that prints all the elements of a list_t list.
+ * @h : head
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	int n;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		n++;
+		h = h->next;
+	}
+	return (n);
+}
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,16 @@
+#ifndef LISTS_H
+#define LISTS_H
+/**
+ * struct list - singly linked list
+ * @str : pointer to a string
+ * @len : length of the string
+ * @next : pointer to next
+ */
+typedef struct list
+{
+	char *str;
+	int len;
+	struct list *next;
+} list_t;
+size_t print_list(const list_t *h);
+#endif;
