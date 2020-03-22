cf972bf83af3fb14f58cd8fe85169d5f10467bbe
achrefbs <siachrefboularess@gmail.com>
2020-03-22 12:48:23 +0000

    ex2 update

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,6 +1,7 @@
 #include "lists.h"
 #include <string.h>
 #include <stdlib.h>
+int strlength(const char *s);
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *s;
@@ -10,6 +11,14 @@ list_t *add_node(list_t **head, const char *str)
 
 	s->next = *head;
 	s->str = strdup(str);
+	s->len = strlength(str);
 	*head = s;
 	return (s);
+}
+int strlength(const char *s)
+{
+	int i;
+	for(i = 0; s[i] != '\0'; i++)
+	;
+	return(i);
 }
