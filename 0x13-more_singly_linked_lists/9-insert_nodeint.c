b33a6e20bb2214fc1af1d4bf7e5d8598a5d1b913
achrefbs <siachrefboularess@gmail.com>
2020-03-24 16:15:50 +0000

    ex9 update

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -8,6 +8,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 	return (NULL);
+	if (*head == NULL)
+	{
+		*head = new;
+		new->n = n;
+		new->next = NULL;
+		return (new);
+	}
 	for (i = 1; i < idx && t->next != NULL; i++)
 	t = t->next;
 	if (i < idx)
