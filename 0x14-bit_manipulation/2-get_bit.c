d8fc868d4e460f66eb879450a0d72c64a945816b
achrefbs <siachrefboularess@gmail.com>
2020-03-27 08:59:33 +0000

    ex2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,4 +1,17 @@
+/**
+ * get_bit - returns the value of a bit at a given index.
+ * @n : unsigned long int
+ * @index : unsigned int
+ * Return: the value of a bit at a given index.
+ */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	
+	if (sizeof(n) * 8 < index)
+	return (-1);
+
+	n >>= index;
+	if ((n & 1) == 1)
+	return (1);
+	else
+	return (0);
 }
--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -2,5 +2,6 @@
 #define HOLBERTON_H
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
+int get_bit(unsigned long int n, unsigned int index);
 void print_binary(unsigned long int n);
 #endif
