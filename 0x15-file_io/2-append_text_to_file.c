6a5464c01e859d09ed98c9b304e825400551ac88
achrefbs <siachrefboularess@gmail.com>
2020-03-31 15:04:53 +0000

    ex2 update

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -3,9 +3,16 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+/**
+ * append_text_to_file - appends text at the end of a file.
+ * @filename : pointer to a string
+ * @text_content : pointer to a string
+ * Return: 1 or -1
+ */
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, ww, l;
+
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_APPEND | O_WRONLY);
--- 0x15-file_io/README.md
+++ 0x15-file_io/README.md
@@ -1,2 +1,3 @@
-1 - a function that reads a text file and prints it to the POSIX standard output.
+0 - a function that reads a text file and prints it to the POSIX standard output.
 1 - a function that creates a file.
+2 - a function that appends text at the end of a file.
