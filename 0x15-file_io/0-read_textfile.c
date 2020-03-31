deecbe89a6ae9c9d2ced64f03721a88b77ed388f
achrefbs <siachrefboularess@gmail.com>
2020-03-31 11:55:43 +0000

    ex0 update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,20 +3,29 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include "holberton.h"
+/**
+ * read_textfile -  reads a text file and prints it to the stdo.
+ * @filename : pointer to a string
+ * @letters : byte count
+ * Return: bytes printed
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd, wr, ww;
-	char* buff = malloc(sizeof(char) * (letters));
+	char *buff = malloc(sizeof(char) * (letters));
+
 	if (filename == NULL)
 		return (0);
 	fd = open(filename, O_RDWR);
 	if (fd == -1)
 		return (0);
-	if((wr = read(fd, buff, letters)) == -1)
+	wr = read(fd, buff, letters);
+	if (wr == -1)
 		return (0);
-	if((ww = write(STDOUT_FILENO, buff, wr)) == -1)
+	ww = write(STDOUT_FILENO, buff, wr);
+	if (ww == -1)
 		return (0);
-	
 	close(fd);
 	free(buff);
 	return (ww);
