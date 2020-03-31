21c10107437c7b5e57f531a888ae67637c2b2213
achrefbs <siachrefboularess@gmail.com>
2020-03-31 17:31:05 +0000

    ex3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,7 +6,7 @@
 #include <stdio.h>
 int main(int ac, char *av[])
 {
-	int fd ,rw, cw;
+	int fd ,rw, cw, l;
 	int fd2, ww, cw2;
 	char *buff = malloc(sizeof(char) * 1024);
 	if (ac != 3)
@@ -15,24 +15,22 @@ int main(int ac, char *av[])
 		exit(97);
 	}
 	fd = open(av[1],O_RDONLY);
-	if (fd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
 	rw = read(fd, buff, 1024);
-	if (rw == -1)
+	if (fd == -1 || rw == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
 	cw = close(fd);
+	for (l = 0; buff[l] != '\0'; l++)
+	;
 	if (cw == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 	}
+
 	fd2 = open(av[2],O_WRONLY | O_TRUNC | O_CREAT, 00664);
-	ww = write(fd2, buff, 1024);
+	ww = write(fd2, buff, l);
 	if (fd2 == -1 || ww == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
