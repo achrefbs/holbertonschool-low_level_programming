cbe2b5d5745715f0865924bc7b53f1478aac5150
achrefbs <siachrefboularess@gmail.com>
2020-03-31 17:25:00 +0000

    ex3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,43 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+int main(int ac, char *av[])
+{
+	int fd ,rw, cw;
+	int fd2, ww, cw2;
+	char *buff = malloc(sizeof(char) * 1024);
+	if (ac != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	fd = open(av[1],O_RDONLY);
+	rw = read(fd, buff, 1024);
+	if (fd == -1 || rw == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+	cw = close(fd);
+	if (cw == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+	}
+	fd2 = open(av[2],O_WRONLY | O_TRUNC | O_CREAT, 00664);
+	ww = write(fd2, buff, 1024);
+	if (fd2 == -1 || ww == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+	cw2 = close(fd2);
+	if (cw2 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
+	}
+	free(buff);
+	return (0);
+}
