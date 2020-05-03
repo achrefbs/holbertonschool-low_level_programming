5a3efaccc753e512d84d936231c43e0963a7769d
achrefbs <siachrefboularess@gmail.com>
2020-05-03 12:50:36 +0000

    ex4 update

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 /**
- * dlistint_len - returns the number of elements in a linked dlistint_t list.
- * @h : head
- * Return: number of nodes
+ * free_dlistint - a function that free a dlistint_t list.
+ * @head : head
  */
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *tmp;
+
 	if (head == NULL)
 		return;
 	while (head)
