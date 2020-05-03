c4c63362ed52fb751a12df7f078136939a6056fa
achrefbs <siachrefboularess@gmail.com>
2020-05-03 13:39:23 +0000

    ex7

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new, *tmp;
+	unsigned int i = 0;
+	tmp = *h;
+
+	while (tmp)
+	{
+		tmp = tmp->next;
+		i++;
+		if(i == idx - 1)
+		break;
+		else if (i < idx - 1 && h == NULL)
+		return(NULL);
+	}
+	new = malloc(sizeof(dlistint_t));
+	if(new == NULL)
+		return (NULL);
+
+	tmp->next->prev = new;
+	new->next = tmp->next;
+	tmp->next = new;
+	new->prev = tmp;
+	new->n = n;
+	return new;
+	
+	
+}
