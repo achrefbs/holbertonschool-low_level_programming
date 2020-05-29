bf87a9b8626cd49785fdf5b37a1e65c6585e6b9c
achrefbs <siachrefboularess@gmail.com>
2020-05-29 13:49:34 +0000

    test

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -5,22 +5,17 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
     unsigned long int idx;
     hash_node_t *node;
-    if (strcmp(key, "") || key == NULL)
+    if (key == NULL)
         return 0;
     idx = key_index((const unsigned char *)key, ht->size);
-    /*
-    while (ht->array[idx])
-    {
-         code 
-    }
-    */
+
     node = malloc(sizeof(hash_node_t));
     if (node == NULL)
         return 0;
    
 
-    node->key = (char *)key;
-    node->value = (char *)value;
+    node->key = (char *)(key);
+    node->value = strdup(value);
     node->next = ht->array[idx];
 
 
