357630daa4689bfcdcf8dd56f541988948717cef
achrefbs <siachrefboularess@gmail.com>
2020-05-29 15:28:23 +0000

    ex3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -3,38 +3,43 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    unsigned long int idx;
-    hash_node_t *node;
-    if (key == NULL)
-        return 0;
-    idx = key_index((const unsigned char *)key, ht->size);
-    /*
-    if (ht->array[idx])
-    while(ht->array[idx])
-    {
-        if (ht->array[idx] && strcmp(ht->array[idx]->key, key) == 0)
-        {
-            free(ht->array[idx]->value);
-            ht->array[idx]->value = strdup(value);
-            if (ht->array[idx]->value == NULL)
-                return 0;
-            return 1;
-        }
-        if (ht->array[idx])
-        ht->array[idx] = ht->array[idx]->next;
-    }*/
+	unsigned long int idx;
+	hash_node_t *node, *tmp;
+	if (key == NULL)
+		return 0;
+	idx = key_index((const unsigned char *)key, ht->size);
 
-    node = malloc(sizeof(hash_node_t));
-    if (node == NULL)
-    return 0;
-    node->key = strdup(key);
-    if (node->key == NULL)
-    return 0;
-    node->value = strdup(value);
-    if (node->value == NULL)
-    return 0;
-    node->next = ht->array[idx];
-    ht->array[idx] = node;
-    return 1;
+	tmp = ht->array[idx];	
+	while (tmp)
+	{
+		if (tmp && strcmp(tmp->key, key) == 0)
+		{
+			free(tmp->value);
+			tmp->value = strdup(value);
+			if (tmp->value == NULL)
+				return 0;
+			return 1;
+		}
+		tmp = tmp->next;
+	}
+	
 
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+	return 0;
+	node->key = strdup(key);
+	if (node->key == NULL)
+	{
+		free(node->key);
+		return 0;
+	}
+	node->value = strdup(value);
+	if (node->value == NULL)
+	{
+		free(node->value);
+		return 0;
+	}
+	node->next = ht->array[idx];
+	ht->array[idx] = node;
+	return 1;
 }
