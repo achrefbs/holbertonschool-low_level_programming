ed07663cec9e911e64dc2128a59825f1aa7310f8
achrefbs <siachrefboularess@gmail.com>
2020-05-29 15:03:48 +0000

    test

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -25,7 +25,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
     node = malloc(sizeof(hash_node_t));
     if (node == NULL)
     return 0;
-    node->key = (char *)(key);
+    node->key = strdup(key);
     node->value = strdup(value);
     node->next = ht->array[idx];
     ht->array[idx] = node;
